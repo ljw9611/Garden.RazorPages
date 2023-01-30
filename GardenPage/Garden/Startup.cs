66a30b548439a7d9fe1cf5aff42aa4e520d09b29
Garden <66628342+ljw9611@users.noreply.github.com>
2023-01-30 17:27:36 +0900

    07 UseStaticFiles 메서드를 사용하여 wwwroot 폴더의 정적인 페이지가 실행되도록 설정

--- GardenPage/Garden/Startup.cs
+++ GardenPage/Garden/Startup.cs
@@ -16,7 +16,7 @@ namespace Garden
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages(); // 해당 메서드 추가
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,12 +26,15 @@ namespace Garden
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            // 미들웨어 추가
+            app.UseStaticFiles(); // wwwroot에 있는 정적인 HTML, CSS, Javascript, 이미지 등 실행을 위해 필요한 메소드
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapRazorPages(); // 해당 메서드 추가
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
