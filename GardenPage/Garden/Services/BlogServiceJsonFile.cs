122a70848e09767dd2e7165110f4126abfe6b3ae
Garden <66628342+ljw9611@users.noreply.github.com>
2023-02-08 02:25:17 +0900

    19. BlogServiceJsonFile 서비스 클래스를 DI Container에 등록하고 필요한 곳에서 주입해서 사용

--- GardenPage/Garden/Services/BlogServiceJsonFile.cs
+++ GardenPage/Garden/Services/BlogServiceJsonFile.cs
@@ -1,4 +1,5 @@
 ﻿using Garden.Models;
+using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,12 +9,28 @@ using System.Text.Json;
 namespace Garden.Services
 {
     public class BlogServiceJsonFile
-    {
-        public IEnumerable<Blog> GetBlogs()
-        {
-            var jsonFileName = @"C:\Garden.RazorPages\Garden.RazorPages\GardenPage\Garden\wwwroot\Blogs\Blogs.json";
+    {   // IWebHostEnvironment의 인스턴스는 _webHostEnvironment 된다.
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-            using (var jsonFileReader = File.OpenText(jsonFileName))
+        public BlogServiceJsonFile(IWebHostEnvironment webHostEnvironment)
+        {
+            this._webHostEnvironment = webHostEnvironment;
+        }
+        // (2)
+        private string JsonFileName // 해당 인스턴스는 아래의 jsonFileName 의 경로 값과 동일하다.
+        {
+            get
+            {
+                //return _webHostEnvironment.WebRootPath + "\\Blogs" + "\\Blogs.json";
+                return Path.Combine(_webHostEnvironment.WebRootPath, "Blogs", "Blogs.json");
+            }
+        }
+
+        public IEnumerable<Blog> GetBlogs()
+        {   // (1)
+            var jsonFileName = @"C:\Garden.RazorPages\Garden.RazorPages\GardenPage\Garden\wwwroot\Blogs\Blogs.json";
+            // (1) jsonFileName 에서 -> (2) JsonFileName으로 변경해서 입력해보기
+            using (var jsonFileReader = File.OpenText(JsonFileName))
             {
                 var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
                 var blogs = JsonSerializer.Deserialize<Blog[]>(jsonFileReader.ReadToEnd(), options);
