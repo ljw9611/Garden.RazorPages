fbd3d4969a4a9420c74e68a7245864fde785131d
Garden <66628342+ljw9611@users.noreply.github.com>
2023-02-08 00:36:10 +0900

    18. JSON 파일을 읽어들여 컬렉션으로 변환해주는 서비스 클래스 만들고 실제로 사용하기

---
+++ GardenPage/Garden/Services/BlogServiceJsonFile.cs
@@ -0,0 +1,24 @@
+﻿using Garden.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+// Json 파일을 읽어들여 컬렌션으로 변환해주는 서비스 클래스
+namespace Garden.Services
+{
+    public class BlogServiceJsonFile
+    {
+        public IEnumerable<Blog> GetBlogs()
+        {
+            var jsonFileName = @"C:\Garden.RazorPages\Garden.RazorPages\GardenPage\Garden\wwwroot\Blogs\Blogs.json";
+
+            using (var jsonFileReader = File.OpenText(jsonFileName))
+            {
+                var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                var blogs = JsonSerializer.Deserialize<Blog[]>(jsonFileReader.ReadToEnd(), options);
+                return blogs;
+            }
+        }
+    }
+}
