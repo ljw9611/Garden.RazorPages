f62aa410aeaedb666e83f94e6ad44349dc5431e8
Garden <66628342+ljw9611@users.noreply.github.com>
2023-02-08 15:17:05 +0900

    20. 비하인드 파일을 갖는 Razor 페이지 추가 (Portfolio Page)

---
+++ GardenPage/Garden/Pages/Protfolio/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Garden.Pages.Protfolio
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
